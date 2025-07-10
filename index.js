0e73a8d35716b6674a4a823c9881fea793c6a9b5
marsnewbie <marsnewbie6655@gmail.com>
2025-07-10 21:13:35 +1200

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const storeHours = {
       1: null,
       2: { open: "11:00", close: "22:00" },
       3: { open: "11:00", close: "22:30" },
-      4: { open: "17:00", close: "22:30" },
-      5: { open: "16:00", close: "23:30" },
-      6: { open: "16:00", close: "23:30" }
+      4: { open: "11:00", close: "22:30" },
+      5: { open: "11:00", close: "23:30" },
+      6: { open: "11:00", close: "23:30" }
     }
   },
   fuhua: {
@@ -32,9 +32,9 @@ const storeHours = {
       1: { open: "11:00", close: "21:00" },
       2: { open: "16:00", close: "21:00" },
       3: { open: "11:00", close: "23:00" },
-      4: { open: "11:00", close: "21:00" },
-      5: { open: "11:00", close: "21:00" },
-      6: { open: "11:00", close: "21:00" }
+      4: { open: "11:00", close: "23:00" },
+      5: { open: "11:00", close: "23:00" },
+      6: { open: "11:00", close: "23:00" }
     }
   }
 };
