4820e0d50b3cf904c1ce71a3b5f3f2d39f94313f
marsnewbie <marsnewbie6655@gmail.com>
2025-08-09 23:03:23 +1200

    Update index.js
    add newjadecity

--- index.js
+++ index.js
@@ -27,6 +27,18 @@ const storeHours = {
       6: { open: "11:00", close: "23:30" }
     }
   },
+  newjadecity: {
+    timezone: "Europe/London",
+    hours: {
+      0: { open: "11:00", close: "23:30" },
+      1: null,
+      2: { open: "11:00", close: "23:00" },
+      3: { open: "11:00", close: "23:30" },
+      4: { open: "11:00", close: "23:30" },
+      5: { open: "11:00", close: "23:30" },
+      6: { open: "11:00", close: "23:30" }
+    }
+  },
   fuhua: {
     timezone: "Pacific/Auckland",
     hours: {
